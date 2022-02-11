91dfb82109247dee94c87b35c0eee58fbbd4218e
Frank Luan <lsf@berkeley.edu>
2022-02-11 06:07:37 -0800

    Sky init and cloud credentials for storage (#102)
    * sky init
    
    * fixes and lint
    
    * Create test_enabled_clouds.py
    
    * Optimizer is now aware of enabled_clouds
    
    * Fix pytest
    
    * Update registry.py
    
    * Support GCS buckets
    
    * Make GCS on GCP work
    
    * yapf behaves differently across versions...
    
    * yapf pls
    
    * Fix Azure
    
    * tweak messages
    
    * tweak
    
    * Apply hotfix from #127
    
    * Simple fixes
    
    * Use monkeypatch
    
    * Address comments
    
    * get rid of Task.enabled_clouds
    
    * fix test
    
    * Always install aws and gcloud utils
    
    * Address comments
    
    * oops
    
    * Revert "Always install aws and gcloud utils"
    
    This reverts commit a4630b1cfd3fd83400c4ad6e8b89a0b8e1326c16.
    
    * Refactor and trigger `sky init` automatically
    
    * Reverted to `is_directory`
    
    * nits
    
    * better check
    
    * usability
    
    * fix tests
    
    * nits
    
    * Address latest comments
    
    * Update init.py
    
    * Fix CLI messages
    
    * Sync credentials regardless of storage mounts
    
    * Fix cpunode and more docs
    
    * Apply changes to requirements
    
    * Update setup.py
    
    * Fix tests
    
    * Fixes
    
    * Add links, fix test
    
    Co-authored-by: Michael Luo <michael.luo@berkeley.edu>
    Co-authored-by: Zongheng Yang <zongheng.y@gmail.com>

--- prototype/sky/task.py
+++ prototype/sky/task.py
@@ -129,9 +129,6 @@ class Task:
         # Filled in by the optimizer.  If None, this Task is not planned.
         self.best_resources = None
 
-        # Block some of the clouds.
-        self.blocked_clouds = set()
-
         # Semantics.
         if num_nodes is not None and num_nodes > 1 and isinstance(
                 self.run, str):
@@ -349,7 +346,6 @@ class Task:
             storage_type = storage_plans[store]
             if storage_type is storage_lib.StorageType.S3:
                 # TODO: allow for Storage mounting of different clouds
-                self.update_file_mounts({'~/.aws': '~/.aws'})
                 self.update_file_mounts({
                     mnt_path: 's3://' + store.name,
                 })
@@ -366,6 +362,7 @@ class Task:
                     mnt_path: 'gs://' + store.name,
                 })
             elif storage_type is storage_lib.StorageType.AZURE:
+                # TODO when Azure Blob is done: sync ~/.azure
                 assert False, 'TODO: Azure Blob not mountable yet'
             else:
                 raise ValueError(f'Storage Type {storage_type} \
@@ -426,11 +423,6 @@ class Task:
         self.file_mounts.update(file_mounts)
         # For validation logic:
         return self.set_file_mounts(self.file_mounts)
-
-    def set_blocked_clouds(self, blocked_clouds: Set[clouds.Cloud]):
-        """Sets the clouds that this task should not run on."""
-        self.blocked_clouds = blocked_clouds
-        return self
 
     def get_local_to_remote_file_mounts(self) -> Optional[Dict[str, str]]:
         """Returns file mounts of the form (dst=VM path, src=local path).

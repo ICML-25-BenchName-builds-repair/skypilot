1d1f315e2d1e512da458c9f0b7fa8e4b3554584d
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-02-23 21:50:52 -0800

    Add document for `bash script.sh` cannot do `conda activate` problem (#422)
    * Add conda activate support to bashrc
    
    * Add doc and make sure conda activate works
    
    * bring back conda activate command for GCP
    
    * Move comment to quickstart
    
    * format
    
    * Fix comments
    
    * Add test/example of using user_script
    
    * Fix indents
    
    * bash -i only for conda activate
    
    * Fix the SKY_NODE_IPS fail to pass to the shell script
    
    * Update readme
    
    * update env_check
    
    * Fix comments
    
    * Change to head -n1

--- examples/resnet_distributed_torch_scripts/setup.sh
+++ examples/resnet_distributed_torch_scripts/setup.sh
@@ -7,11 +7,10 @@ if [ $? -eq 0 ]; then
     echo "conda env exists"
 else
     echo "conda env does not exist"
-    conda create -n resnet python=3.7 -y
+    conda create -n resnet python=3.6 -y
     conda activate resnet
 fi
-# SkyPilot's default image on AWS/GCP has CUDA 11.6 (Azure 11.5).
-pip install -r requirements.txt torch==1.12.1+cu113 --extra-index-url https://download.pytorch.org/whl/cu113
+pip install -r requirements.txt
 
 mkdir -p data
 mkdir -p saved_models

babc67e6e6a107e4b7ce628f431343a91cb6b5a6
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-03-25 14:04:48 -0700

    Add a skylet daemon and fix job status problem (#623)
    * Refactor JobLibCodeGen and fix job status update logic
    
    * Fix job_lib
    
    * fix job lib again
    
    * Add comment
    
    * fix job_lib
    
    * fix update_status
    
    * Fix INIT status update
    
    * format
    
    * add daemon
    
    * fix
    
    * Add comment
    
    * start skylet
    
    * format
    
    * pylint
    
    * fix skylet start in template
    
    * Fix job fail status in ray job
    
    * Fix second `sky launch`
    
    * format
    
    * fix skylet checking in the test
    
    * fix skylet launching
    
    * remove -v for ray up
    
    * format
    
    * address comments
    
    * Only teardown the cluster when test succeeded
    
    * fix space

--- sky/skylet/skylet.py
+++ sky/skylet/skylet.py
@@ -2,28 +2,10 @@
 
 import time
 
-import sky
-from sky import sky_logging
-from sky.skylet import constants
-from sky.skylet import events
+from sky.skylet import job_lib
 
-# Use the explicit logger name so that the logger is under the
-# `sky.skylet.skylet` namespace when executed directly, so as
-# to inherit the setup from the `sky` logger.
-logger = sky_logging.init_logger('sky.skylet.skylet')
-logger.info(f'Skylet started with version {constants.SKYLET_VERSION}; '
-            f'SkyPilot v{sky.__version__} (commit: {sky.__commit__})')
-
-EVENTS = [
-    events.AutostopEvent(),
-    events.JobSchedulerEvent(),
-    # The spot job update event should be after the job update event.
-    # Otherwise, the abnormal spot job status update will be delayed
-    # until the next job update event.
-    events.SpotJobUpdateEvent(),
-]
-
+timestamp = time.strftime('[%Y-%m-%d %H:%M:%S]', time.localtime())
+print(f'[{timestamp}] skylet started')
 while True:
-    time.sleep(events.EVENT_CHECKING_INTERVAL_SECONDS)
-    for event in EVENTS:
-        event.run()
+    time.sleep(20)
+    job_lib.update_status()

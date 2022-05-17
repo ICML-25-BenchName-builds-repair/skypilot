084e29a3c6ba418452b8133c913f24dede630a77
Wei-Lin Chiang <weichiang@berkeley.edu>
2022-05-16 21:27:39 -0700

    Fix sky cancel for on-prem mode (#775)
    * Replace ps forest with pstree to handle on-prem
    
    * remove pgid
    
    * Fix
    
    * Add tests for all clouds
    
    * Python based subprocess daemon
    
    * yapf
    
    * rm subprocess_daemon.sh
    
    * comment
    
    * add setup and fix test
    
    * replace workdir with git clone for test_distributed_tf
    
    * Fix sky spot issue
    
    * Needs more time for sky cancel to work
    
    * Fix bug
    
    * address comments

--- sky/skylet/subprocess_daemon.py
+++ sky/skylet/subprocess_daemon.py
@@ -5,65 +5,29 @@ processes of proc_pid.
 """
 
 import argparse
-import sys
 import time
+import sys
 
 import psutil
-from ray.dashboard.modules.job import common as job_common
-from ray.dashboard.modules.job import sdk as job_sdk
-import requests
-
-from sky.skylet import job_lib
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--parent-pid', type=int, required=True)
     parser.add_argument('--proc-pid', type=int, required=True)
-    parser.add_argument('--local-ray-job-id', type=str, required=False)
     args = parser.parse_args()
-    local_ray_job_id = args.local_ray_job_id
 
-    process = None
-    parent_process = None
-    try:
-        process = psutil.Process(args.proc_pid)
-        parent_process = psutil.Process(args.parent_pid)
-    except psutil.NoSuchProcess:
-        pass
+    parent_process = psutil.Process(args.parent_pid)
+    process = psutil.Process(args.proc_pid)
 
-    if process is None:
+    if process is None or parent_process is None:
         sys.exit()
 
-    wait_for_process = False
-    # Local Ray Job ID is only used in the Sky On-prem case.
-    # If Local Ray job id is passed in, wait until the job
-    # is done/cancelled/failed.
-    if local_ray_job_id is None:
-        wait_for_process = True
-    else:
-        try:
-            # Polls the Job submission client to check job status.
-            port = job_lib.get_job_submission_port()
-            client = job_sdk.JobSubmissionClient(f'http://127.0.0.1:{port}')
-            while True:
-                status_info = client.get_job_status(local_ray_job_id)
-                status = status_info.status
-                if status in {
-                        job_common.JobStatus.SUCCEEDED,
-                        job_common.JobStatus.STOPPED,
-                        job_common.JobStatus.FAILED
-                }:
-                    break
-                time.sleep(1)
-        except (requests.exceptions.ConnectionError, RuntimeError) as e:
-            print(e)
-            wait_for_process = True
-
-    if wait_for_process and parent_process is not None:
-        # Wait for either parent or target process to exit.
-        while process.is_running() and parent_process.is_running():
-            time.sleep(1)
+    # Wait for either parent or target process to exit.
+    while True:
+        time.sleep(1)
+        if not process.is_running() or not parent_process.is_running():
+            break
 
     try:
         children = process.children(recursive=True)

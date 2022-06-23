16ff3ede9451a380d0aeb7668a3d995bc975bce9
Romil Bhardwaj <romil.bhardwaj@gmail.com>
2022-06-23 09:02:42 +0530

    Mount mode support for GCS (#918)
    * add gcsfuse
    
    * add gcsfuse
    
    * add gs tests and update docs
    
    * address comments
    
    * fix
    
    * lint
    
    * tmp_mount to tmp_source

--- sky/data/mounting_utils.py
+++ sky/data/mounting_utils.py
@@ -1,107 +1,66 @@
 """Helper functions for object store mounting in Sky Storage"""
 import random
 import textwrap
-from typing import Optional
-
-from sky import exceptions
-from sky.data import storage_utils
 
 
 def get_mounting_command(
-    mount_mode: storage_utils.StorageMode,
     mount_path: str,
+    install_cmd: str,
     mount_cmd: str,
-    install_cmd: Optional[str] = None,
-    version_check_cmd: Optional[str] = None,
-    csync_log_path: Optional[str] = None,
 ) -> str:
-    """Generates the mounting command for a given bucket.
-
-    There are two types of mounting supported in Skypilot, a traditional MOUNT
-    and CSYNC.
-
-    For traditional mounting, generated script first unmounts any
-    existing mount at the mount path, checks and installs the mounting utility
-    if required, creates the mount path and finally mounts the bucket.
-
-    For CSYNC, generated script first creates the CSYNC_PATH if it does not
-    exist, and finally runs CSYNC daemon on CSYNC_PATH to the bucket.
+    """
+    Generates the mounting command for a given bucket. Generated script first
+    unmounts any existing mount at the mount path, checks and installs the
+    mounting utility if required, creates the mount path and finally mounts
+    the bucket.
 
     Args:
-        mount_mode: Defines which mounting mode is used between traditional
-          MOUNT and CSYNC
         mount_path: Path to mount the bucket at.
+        install_cmd: Command to install the mounting utility. Should be
+          single line.
         mount_cmd: Command to mount the bucket. Should be single line.
-        install_cmd: Command to install the mounting utility for MOUNT mode.
-          Should be single line.
 
     Returns:
         str: Mounting command with the mounting script as a heredoc.
     """
     mount_binary = mount_cmd.split()[0]
-    installed_check = f'[ -x "$(command -v {mount_binary})" ]'
-    if mount_mode == storage_utils.StorageMode.MOUNT:
-        script_path = f'~/.sky/mount_{random.randint(0, 1000000)}.sh'
-        assert csync_log_path is None, ('CSYNC log path should '
-                                        'not be defined for MOUNT mode.')
-        if version_check_cmd is not None:
-            installed_check += f' && {version_check_cmd}'
-    else:  # script path for CSYNC mode
-        script_path = f'~/.sky/csync_{random.randint(0, 1000000)}.sh'
-        assert install_cmd is None, ('Installing commands should '
-                                     'not be defined for CSYNC mode.')
-
     script = textwrap.dedent(f"""
         #!/usr/bin/env bash
         set -e
 
-        MOUNT_MODE={mount_mode.value}
-        MOUNT_PATH='{mount_path}'
-        echo "MOUNT_MODE is: $MOUNT_MODE"
+        MOUNT_PATH={mount_path}
+        MOUNT_BINARY={mount_binary}
 
-        if [ "$MOUNT_MODE" = "MOUNT" ]; then
-            MOUNT_BINARY={mount_binary}
-            # Check if path is already mounted
-            if grep -q $MOUNT_PATH /proc/mounts ; then
-              echo "Path already mounted - unmounting..."
-              fusermount -uz "$MOUNT_PATH"
-              echo "Successfully unmounted $MOUNT_PATH."
-            fi
+        # Check if path is already mounted
+        if grep -q $MOUNT_PATH /proc/mounts ; then
+            echo "Path already mounted - unmounting..."
+            fusermount -u "$MOUNT_PATH"
+            echo "Successfully unmounted $MOUNT_PATH."
+        fi
 
-            # Install MOUNT_BINARY if not already installed
-            if {installed_check}; then
-              echo "$MOUNT_BINARY already installed. Proceeding..."
-            else
-              echo "Installing $MOUNT_BINARY..."
-              {install_cmd}
-            fi
+        # Install MOUNT_BINARY if not already installed
+        if ! [ -x "$(command -v $MOUNT_BINARY)" ]; then
+          echo "Installing $MOUNT_BINARY..."
+          {install_cmd}
+        else
+          echo "$MOUNT_BINARY already installed. Proceeding..."
         fi
 
         # Check if mount path exists
         if [ ! -d "$MOUNT_PATH" ]; then
-          echo "Mount/CSYNC path $MOUNT_PATH does not exist. Creating..."
+          echo "Mount path $MOUNT_PATH does not exist. Creating..."
           sudo mkdir -p $MOUNT_PATH
           sudo chmod 777 $MOUNT_PATH
         else
-          # Check if mount path contains files for MOUNT mode only
-          if [ "$MOUNT_MODE" = "MOUNT" ]; then
-            if [ "$(ls -A $MOUNT_PATH)" ]; then
-              echo "Mount path $MOUNT_PATH is not empty. Please mount to another path or remove it first."
-              exit {exceptions.MOUNT_PATH_NON_EMPTY_CODE}
-            fi
+          # Check if mount path contains files
+          if [ "$(ls -A $MOUNT_PATH)" ]; then
+            echo "Mount path $MOUNT_PATH is not empty. Please make sure its empty."
+            exit 1
           fi
         fi
-
-        if [ "$MOUNT_MODE" = "MOUNT" ]; then
-          echo "Mounting source bucket to $MOUNT_PATH with $MOUNT_BINARY..."
-          {mount_cmd}
-          echo "Mounting done."
-        else
-          # running CSYNC cmd
-          echo "Setting up CSYNC on $MOUNT_PATH to source bucket..."
-          setsid {mount_cmd} >> {csync_log_path} 2>&1 &
-          echo "CSYNC is set."
-        fi
+        echo "Mounting $SOURCE_BUCKET to $MOUNT_PATH with $MOUNT_BINARY..."
+        {mount_cmd}
+        echo "Mounting done."
     """)
 
     # TODO(romilb): Get direct bash script to work like so:
@@ -116,6 +75,7 @@ def get_mounting_command(
 
     # While these commands are run sequentially for each storage object,
     # we add random int to be on the safer side and avoid collisions.
+    script_path = f'~/.sky/mount_{random.randint(0, 1000000)}.sh'
     first_line = r'(cat <<-\EOF > {}'.format(script_path)
     command = (f'{first_line}'
                f'{script}'

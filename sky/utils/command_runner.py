cb9cf0fb6d69c90809ad6af3ab320ff1cf8e51cc
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-06-27 23:33:50 -0700

    Refactor the ssh/rsync commands (#936)
    * Add missing schemas
    
    * remove stderr from rary get-head-ip
    
    * format
    
    * refactor ssh commands
    
    * Fix functions
    
    * address comments
    
    * fix
    
    * Add example
    
    * Merge branch 'master' of github.com:concretevitamin/sky-experiments into refactor-rsync
    
    * Fix parallel run
    
    * remove unavailable s3 from the managed_spot_with_storage
    
    * address comments
    
    * hide function
    
    * format
    
    * address comments
    
    * rename
    
    * remove control name from backend
    
    * rename
    
    * Fix library
    
    * reduce the length of control name
    
    * change back to not using environment variable
    
    * format
    
    * Add a blank line after confirm

--- sky/utils/command_runner.py
+++ sky/utils/command_runner.py
@@ -1,18 +1,15 @@
 """Runner for commands to be executed on the cluster."""
-import enum
 import getpass
+import enum
 import hashlib
 import os
 import pathlib
 import shlex
-import time
 from typing import List, Optional, Tuple, Union
 
 from sky import sky_logging
-from sky.skylet import constants
-from sky.skylet import log_lib
-from sky.utils import common_utils
 from sky.utils import subprocess_utils
+from sky.skylet import log_lib
 
 logger = sky_logging.init_logger(__name__)
 
@@ -37,27 +34,19 @@ def _ssh_control_path(ssh_control_filename: Optional[str]) -> Optional[str]:
     if ssh_control_filename is None:
         return None
     username = getpass.getuser()
-    path = (f'/tmp/skypilot_ssh_{username}/{ssh_control_filename}')
+    path = (f'/tmp/sky_ssh_{username}/{ssh_control_filename}')
     os.makedirs(path, exist_ok=True)
     return path
 
 
-def ssh_options_list(
-    ssh_private_key: Optional[str],
-    ssh_control_name: Optional[str],
-    *,
-    ssh_proxy_command: Optional[str] = None,
-    docker_ssh_proxy_command: Optional[str] = None,
-    timeout: int = 30,
-    port: int = 22,
-    disable_control_master: Optional[bool] = False,
-) -> List[str]:
+def _ssh_options_list(ssh_private_key: Optional[str],
+                      ssh_control_name: Optional[str],
+                      *,
+                      timeout=30) -> List[str]:
     """Returns a list of sane options for 'ssh'."""
     # Forked from Ray SSHOptions:
     # https://github.com/ray-project/ray/blob/master/python/ray/autoscaler/_private/command_runner.py
     arg_dict = {
-        # SSH port
-        'Port': port,
         # Supresses initial fingerprint verification.
         'StrictHostKeyChecking': 'no',
         # SSH IP and fingerprint pairs no longer added to known_hosts.
@@ -80,41 +69,18 @@ def ssh_options_list(
         # Agent forwarding for git.
         'ForwardAgent': 'yes',
     }
-    # SSH Control will have a severe delay when using docker_ssh_proxy_command.
-    # TODO(tian): Investigate why.
-    # We also do not use ControlMaster when we use `kubectl port-forward`
-    # to access Kubernetes pods over SSH+Proxycommand. This is because the
-    # process running ProxyCommand is kept running as long as the ssh session
-    # is running and the ControlMaster keeps the session, which results in
-    # 'ControlPersist' number of seconds delay per ssh commands ran.
-    if (ssh_control_name is not None and docker_ssh_proxy_command is None and
-            not disable_control_master):
+    if ssh_control_name is not None:
         arg_dict.update({
             # Control path: important optimization as we do multiple ssh in one
             # sky.launch().
             'ControlMaster': 'auto',
             'ControlPath': f'{_ssh_control_path(ssh_control_name)}/%C',
-            'ControlPersist': '300s',
+            'ControlPersist': '120s',
         })
     ssh_key_option = [
         '-i',
         ssh_private_key,
     ] if ssh_private_key is not None else []
-
-    if docker_ssh_proxy_command is not None:
-        logger.debug(f'--- Docker SSH Proxy: {docker_ssh_proxy_command} ---')
-        arg_dict.update({
-            'ProxyCommand': shlex.quote(docker_ssh_proxy_command),
-        })
-
-    if ssh_proxy_command is not None:
-        logger.debug(f'--- Proxy: {ssh_proxy_command} ---')
-        arg_dict.update({
-            # Due to how log_lib.run_with_log() works (using shell=True) we
-            # must quote this value.
-            'ProxyCommand': shlex.quote(ssh_proxy_command),
-        })
-
     return ssh_key_option + [
         x for y in (['-o', f'{k}={v}']
                     for k, v in arg_dict.items()
@@ -141,18 +107,14 @@ class SSHCommandRunner:
         ip: str,
         ssh_user: str,
         ssh_private_key: str,
-        ssh_control_name: Optional[str] = '__default__',
-        ssh_proxy_command: Optional[str] = None,
-        port: int = 22,
-        docker_user: Optional[str] = None,
-        disable_control_master: Optional[bool] = False,
+        ssh_control_name: str = '__default__',
     ):
         """Initialize SSHCommandRunner.
 
         Example Usage:
             runner = SSHCommandRunner(ip, ssh_user, ssh_private_key)
             runner.run('ls -l', mode=SshMode.NON_INTERACTIVE)
-            runner.rsync(source, target, up=True)
+            runner.rsync_up(source, target)
 
         Args:
             ip: The IP address of the remote machine.
@@ -162,60 +124,23 @@ class SSHCommandRunner:
                 used to avoid confliction between clusters for creating ssh
                 control files. It can simply be the cluster_name or any name
                 that can distinguish between clusters.
-            ssh_proxy_command: Optional, the value to pass to '-o
-                ProxyCommand'. Useful for communicating with clusters without
-                public IPs using a "jump server".
-            port: The port to use for ssh.
-            docker_user: The docker user to use for ssh. If specified, the
-                command will be run inside a docker container which have a ssh
-                server running at port sky.skylet.constants.DEFAULT_DOCKER_PORT
-            disable_control_master: bool; specifies either or not the ssh
-                command will utilize ControlMaster. We currently disable
-                it for k8s instance.
         """
+        self.ip = ip
+        self.ssh_user = ssh_user
         self.ssh_private_key = ssh_private_key
-        self.ssh_control_name = (
-            None if ssh_control_name is None else hashlib.md5(
-                ssh_control_name.encode()).hexdigest()[:_HASH_MAX_LENGTH])
-        self._ssh_proxy_command = ssh_proxy_command
-        self.disable_control_master = disable_control_master
-        if docker_user is not None:
-            assert port is None or port == 22, (
-                f'port must be None or 22 for docker_user, got {port}.')
-            # Already checked in resources
-            assert ssh_proxy_command is None, (
-                'ssh_proxy_command is not supported when using docker.')
-            self.ip = 'localhost'
-            self.ssh_user = docker_user
-            self.port = constants.DEFAULT_DOCKER_PORT
-            self._docker_ssh_proxy_command = lambda ssh: ' '.join(
-                ssh + ssh_options_list(ssh_private_key, None
-                                      ) + ['-W', '%h:%p', f'{ssh_user}@{ip}'])
-        else:
-            self.ip = ip
-            self.ssh_user = ssh_user
-            self.port = port
-            self._docker_ssh_proxy_command = None
+        self.ssh_control_name = hashlib.md5(
+            ssh_control_name.encode()).hexdigest()[:_HASH_MAX_LENGTH]
 
     @staticmethod
     def make_runner_list(
-        ip_list: List[str],
-        ssh_user: str,
-        ssh_private_key: str,
-        ssh_control_name: Optional[str] = None,
-        ssh_proxy_command: Optional[str] = None,
-        disable_control_master: Optional[bool] = False,
-        port_list: Optional[List[int]] = None,
-        docker_user: Optional[str] = None,
-    ) -> List['SSHCommandRunner']:
+            ip_list: List[str],
+            ssh_user: str,
+            ssh_private_key: str,
+            ssh_control_name: Optional[str] = None) -> List['SSHCommandRunner']:
         """Helper function for creating runners with the same ssh credentials"""
-        if not port_list:
-            port_list = [22] * len(ip_list)
         return [
-            SSHCommandRunner(ip, ssh_user, ssh_private_key, ssh_control_name,
-                             ssh_proxy_command, port, docker_user,
-                             disable_control_master)
-            for ip, port in zip(ip_list, port_list)
+            SSHCommandRunner(ip, ssh_user, ssh_private_key, ssh_control_name)
+            for ip in ip_list
         ]
 
     def _ssh_base_command(self, *, ssh_mode: SshMode,
@@ -234,33 +159,22 @@ class SSHCommandRunner:
                 logger.info(
                     f'Forwarding port {local} to port {remote} on localhost.')
                 ssh += ['-L', f'{remote}:localhost:{local}']
-        if self._docker_ssh_proxy_command is not None:
-            docker_ssh_proxy_command = self._docker_ssh_proxy_command(ssh)
-        else:
-            docker_ssh_proxy_command = None
-        return ssh + ssh_options_list(
+        return ssh + _ssh_options_list(
             self.ssh_private_key,
-            self.ssh_control_name,
-            ssh_proxy_command=self._ssh_proxy_command,
-            docker_ssh_proxy_command=docker_ssh_proxy_command,
-            port=self.port,
-            disable_control_master=self.disable_control_master) + [
-                f'{self.ssh_user}@{self.ip}'
-            ]
+            self.ssh_control_name) + [f'{self.ssh_user}@{self.ip}']
 
     def run(
             self,
             cmd: Union[str, List[str]],
             *,
-            require_outputs: bool = False,
             port_forward: Optional[List[int]] = None,
             # Advanced options.
+            require_outputs: bool = False,
             log_path: str = os.devnull,
             # If False, do not redirect stdout/stderr to optimize performance.
             process_stream: bool = True,
             stream_logs: bool = True,
             ssh_mode: SshMode = SshMode.NON_INTERACTIVE,
-            separate_stderr: bool = False,
             **kwargs) -> Union[int, Tuple[int, str, str]]:
         """Uses 'ssh' to run 'cmd' on a node with ip.
 
@@ -278,7 +192,6 @@ class SSHCommandRunner:
             check: Check the success of the command.
             ssh_mode: The mode to use for ssh.
                 See SSHMode for more details.
-            separate_stderr: Whether to separate stderr from stdout.
 
 
         Returns:
@@ -310,9 +223,8 @@ class SSHCommandRunner:
         command += [
             shlex.quote(f'true && source ~/.bashrc && export OMP_NUM_THREADS=1 '
                         f'PYTHONWARNINGS=ignore && ({cmd})'),
+            '2>&1',
         ]
-        if not separate_stderr:
-            command.append('2>&1')
         if not process_stream and ssh_mode == SshMode.NON_INTERACTIVE:
             command += [
                 # A hack to remove the following bash warnings (twice):
@@ -324,8 +236,8 @@ class SSHCommandRunner:
                 '; exit ${PIPESTATUS[0]}'
             ]
 
-        command_str = ' '.join(command)
-        command = base_ssh_command + [shlex.quote(command_str)]
+        command = ' '.join(command)
+        command = base_ssh_command + [shlex.quote(command)]
 
         executable = None
         if not process_stream:
@@ -342,39 +254,23 @@ class SSHCommandRunner:
 
         return log_lib.run_with_log(' '.join(command),
                                     log_path,
-                                    require_outputs=require_outputs,
-                                    stream_logs=stream_logs,
+                                    stream_logs,
                                     process_stream=process_stream,
+                                    require_outputs=require_outputs,
                                     shell=True,
                                     executable=executable,
                                     **kwargs)
 
-    def rsync(
+    def rsync_up(
         self,
         source: str,
         target: str,
         *,
-        up: bool,
         # Advanced options.
         log_path: str = os.devnull,
         stream_logs: bool = True,
-        max_retry: int = 1,
-    ) -> None:
-        """Uses 'rsync' to sync 'source' to 'target'.
-
-        Args:
-            source: The source path.
-            target: The target path.
-            up: The direction of the sync, True for local to cluster, False
-              for cluster to local.
-            log_path: Redirect stdout/stderr to the log_path.
-            stream_logs: Stream logs to the stdout/stderr.
-            max_retry: The maximum number of retries for the rsync command.
-              This value should be non-negative.
-
-        Raises:
-            exceptions.CommandError: rsync command failed.
-        """
+    ) -> Union[int, Tuple[int, str, str]]:
+        """TODO(zhwu)"""
         # Build command.
         # TODO(zhwu): This will print a per-file progress bar (with -P),
         # shooting a lot of messages to the output. --info=progress2 is used
@@ -385,69 +281,26 @@ class SSHCommandRunner:
         # --filter
         rsync_command.append(RSYNC_FILTER_OPTION)
 
-        if up:
-            # The source is a local path, so we need to resolve it.
-            # --exclude-from
-            resolved_source = pathlib.Path(source).expanduser().resolve()
-            if (resolved_source / GIT_EXCLUDE).exists():
-                # Ensure file exists; otherwise, rsync will error out.
-                #
-                # We shlex.quote() because the path may contain spaces:
-                #   'my dir/.git/info/exclude'
-                # Without quoting rsync fails.
-                rsync_command.append(
-                    RSYNC_EXCLUDE_OPTION.format(
-                        shlex.quote(str(resolved_source / GIT_EXCLUDE))))
+        # --exclude-from
+        resolved_source = pathlib.Path(source).expanduser().resolve()
+        if (resolved_source / GIT_EXCLUDE).exists():
+            # Ensure file exists; otherwise, rsync will error out.
+            rsync_command.append(
+                RSYNC_EXCLUDE_OPTION.format(str(resolved_source / GIT_EXCLUDE)))
 
-        if self._docker_ssh_proxy_command is not None:
-            docker_ssh_proxy_command = self._docker_ssh_proxy_command(['ssh'])
-        else:
-            docker_ssh_proxy_command = None
         ssh_options = ' '.join(
-            ssh_options_list(
-                self.ssh_private_key,
-                self.ssh_control_name,
-                ssh_proxy_command=self._ssh_proxy_command,
-                docker_ssh_proxy_command=docker_ssh_proxy_command,
-                port=self.port,
-                disable_control_master=self.disable_control_master))
+            _ssh_options_list(self.ssh_private_key, self.ssh_control_name))
         rsync_command.append(f'-e "ssh {ssh_options}"')
-        # To support spaces in the path, we need to quote source and target.
-        # rsync doesn't support '~' in a quoted local path, but it is ok to
-        # have '~' in a quoted remote path.
-        if up:
-            full_source_str = str(resolved_source)
-            if resolved_source.is_dir():
-                full_source_str = os.path.join(full_source_str, '')
-            rsync_command.extend([
-                f'{full_source_str!r}',
-                f'{self.ssh_user}@{self.ip}:{target!r}',
-            ])
-        else:
-            rsync_command.extend([
-                f'{self.ssh_user}@{self.ip}:{source!r}',
-                f'{os.path.expanduser(target)!r}',
-            ])
+        rsync_command.extend([
+            source,
+            f'{self.ssh_user}@{self.ip}:{target}',
+        ])
         command = ' '.join(rsync_command)
 
-        backoff = common_utils.Backoff(initial_backoff=5, max_backoff_factor=5)
-        while max_retry >= 0:
-            returncode, _, stderr = log_lib.run_with_log(
-                command,
-                log_path=log_path,
-                stream_logs=stream_logs,
-                shell=True,
-                require_outputs=True)
-            if returncode == 0:
-                break
-            max_retry -= 1
-            time.sleep(backoff.current_backoff())
-
-        direction = 'up' if up else 'down'
-        error_msg = (f'Failed to rsync {direction}: {source} -> {target}. '
-                     'Ensure that the network is stable, then retry.')
-        subprocess_utils.handle_returncode(returncode,
-                                           command,
-                                           error_msg,
-                                           stderr=stderr,
-                                           stream_logs=stream_logs)
+        returncode = log_lib.run_with_log(command,
+                                          log_path=log_path,
+                                          stream_logs=stream_logs,
+                                          shell=True)
+        subprocess_utils.handle_returncode(
+            returncode, command, f'Failed to rsync up {source} -> {target}, '
+            f'see {log_path} for details.')
--- sky/utils/subprocess_utils.py
+++ sky/utils/subprocess_utils.py
@@ -1,16 +1,12 @@
 """Utility functions for subprocesses."""
 from multiprocessing import pool
-import random
 import subprocess
-import time
-from typing import Any, Callable, List, Optional, Tuple, Union
+from typing import Any, Callable, List, Optional
 
 import colorama
-import psutil
 
 from sky import exceptions
 from sky import sky_logging
-from sky.skylet import log_lib
 from sky.utils import timeline
 from sky.utils import ux_utils
 
@@ -54,9 +50,10 @@ def run_in_parallel(func: Callable, args: List[Any]) -> List[Any]:
         return list(p.imap(func, args))
 
 
+@ux_utils.print_exception_no_traceback_decorator
 def handle_returncode(returncode: int,
                       command: str,
-                      error_msg: Union[str, Callable[[], str]],
+                      error_msg: str,
                       stderr: Optional[str] = None,
                       stream_logs: bool = True) -> None:
     """Handle the returncode of a command.
@@ -71,99 +68,6 @@ def handle_returncode(returncode: int,
     if returncode != 0:
         if stderr is not None:
             echo(stderr)
-
-        if callable(error_msg):
-            error_msg = error_msg()
         format_err_msg = (
             f'{colorama.Fore.RED}{error_msg}{colorama.Style.RESET_ALL}')
-        with ux_utils.print_exception_no_traceback():
-            raise exceptions.CommandError(returncode, command, format_err_msg,
-                                          stderr)
-
-
-def kill_children_processes(first_pid_to_kill: Optional[int] = None,
-                            force: bool = False):
-    """Kill children processes recursively.
-
-    We need to kill the children, so that
-    1. The underlying subprocess will not print the logs to the terminal,
-       after this program exits.
-    2. The underlying subprocess will not continue with starting a cluster
-       etc. while we are cleaning up the clusters.
-
-    Args:
-        first_pid_to_kill: Optional PID of a process to be killed first.
-         This is for guaranteeing the order of cleaning up and suppress
-         flaky errors.
-    """
-    parent_process = psutil.Process()
-    child_processes = []
-    for child in parent_process.children(recursive=True):
-        if child.pid == first_pid_to_kill:
-            try:
-                if force:
-                    child.kill()
-                else:
-                    child.terminate()
-                child.wait()
-            except psutil.NoSuchProcess:
-                # The child process may have already been terminated.
-                pass
-        else:
-            child_processes.append(child)
-
-    for child in child_processes:
-        try:
-            if force:
-                child.kill()
-            else:
-                child.terminate()
-        except psutil.NoSuchProcess:
-            # The child process may have already been terminated.
-            pass
-
-
-def run_with_retries(
-        cmd: str,
-        max_retry: int = 3,
-        retry_returncode: Optional[List[int]] = None,
-        retry_stderrs: Optional[List[str]] = None) -> Tuple[int, str, str]:
-    """Run a command and retry if it fails due to the specified reasons.
-
-    Args:
-        cmd: The command to run.
-        max_retry: The maximum number of retries.
-        retry_returncode: The returncodes that should be retried.
-        retry_stderr: The cmd needs to be retried if the stderr contains any of
-            the strings in this list.
-
-    Returns:
-        The returncode, stdout, and stderr of the command.
-    """
-    retry_cnt = 0
-    while True:
-        returncode, stdout, stderr = log_lib.run_with_log(cmd,
-                                                          '/dev/null',
-                                                          require_outputs=True,
-                                                          shell=True)
-        if retry_cnt < max_retry:
-            if (retry_returncode is not None and
-                    returncode in retry_returncode):
-                retry_cnt += 1
-                time.sleep(random.uniform(0, 1) * 2)
-                continue
-
-            if retry_stderrs is None:
-                break
-
-            need_retry = False
-            for retry_err in retry_stderrs:
-                if retry_err in stderr:
-                    retry_cnt += 1
-                    time.sleep(random.uniform(0, 1) * 2)
-                    need_retry = True
-                    break
-            if need_retry:
-                continue
-        break
-    return returncode, stdout, stderr
+        raise exceptions.CommandError(returncode, command, format_err_msg)

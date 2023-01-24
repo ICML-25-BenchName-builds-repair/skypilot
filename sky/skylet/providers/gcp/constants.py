47d79b0e6bda4dd05377261541340688d70ddff6
Wei-Lin Chiang <infwinston@gmail.com>
2023-01-25 04:50:07 +0800

    Add GCP VPC creation if no VPC available (#1594)
    * add vpc creation
    
    * shorten
    
    * create vpcnet in ray up instead
    
    * implement best effort
    
    * fix
    
    * add constants.py
    
    * fix
    
    * quote..
    
    * comments
    
    * comment
    
    * fix
    
    * backward compatibility
    
    * comment
    
    * list_instances
    
    * fix
    
    * fix

--- sky/skylet/providers/gcp/constants.py
+++ sky/skylet/providers/gcp/constants.py
@@ -9,30 +9,6 @@ VPC_TEMPLATE = {
     "mtu": 1460,
     "routingConfig": {"routingMode": "GLOBAL"},
 }
-# Required firewall rules for SkyPilot to work.
-FIREWALL_RULES_REQUIRED = [
-    # Allow internal connections between GCP VMs for Ray multi-node cluster.
-    {
-        "direction": "INGRESS",
-        "allowed": [
-            {"IPProtocol": "tcp", "ports": ["0-65535"]},
-            {"IPProtocol": "udp", "ports": ["0-65535"]},
-        ],
-        "sourceRanges": ["10.128.0.0/9"],
-    },
-    # Allow ssh connection from anywhere.
-    {
-        "direction": "INGRESS",
-        "allowed": [
-            {
-                "IPProtocol": "tcp",
-                "ports": ["22"],
-            }
-        ],
-        "sourceRanges": ["0.0.0.0/0"],
-    },
-]
-# Template when creating firewall rules for a new VPC.
 FIREWALL_RULES_TEMPLATE = [
     {
         "name": "{VPC_NAME}-allow-custom",
@@ -42,9 +18,9 @@ FIREWALL_RULES_TEMPLATE = [
         "direction": "INGRESS",
         "priority": 65534,
         "allowed": [
-            {"IPProtocol": "tcp", "ports": ["0-65535"]},
-            {"IPProtocol": "udp", "ports": ["0-65535"]},
-            {"IPProtocol": "icmp"},
+            {'IPProtocol': 'tcp', 'ports': ['0-65535']},
+            {'IPProtocol': 'udp', 'ports': ['0-65535']},
+            {'IPProtocol': 'icmp'}
         ],
         "sourceRanges": ["10.128.0.0/9"],
     },
@@ -55,12 +31,10 @@ FIREWALL_RULES_TEMPLATE = [
         "selfLink": "projects/{PROJ_ID}/global/firewalls/{VPC_NAME}-allow-ssh",
         "direction": "INGRESS",
         "priority": 65534,
-        "allowed": [
-            {
-                "IPProtocol": "tcp",
-                "ports": ["22"],
-            }
-        ],
+        "allowed": [{
+            "IPProtocol": "tcp",
+            "ports": ["22"],
+        }],
         "sourceRanges": ["0.0.0.0/0"],
     },
     {
@@ -70,55 +44,9 @@ FIREWALL_RULES_TEMPLATE = [
         "selfLink": "projects/{PROJ_ID}/global/firewalls/{VPC_NAME}-allow-icmp",
         "direction": "INGRESS",
         "priority": 65534,
-        "allowed": [
-            {
-                "IPProtocol": "icmp",
-            }
-        ],
+        "allowed": [{
+            "IPProtocol": "icmp",
+        }],
         "sourceRanges": ["0.0.0.0/0"],
     },
-]
-
-# A list of permissions required to run SkyPilot on GCP.
-# Keep this in sync with https://skypilot.readthedocs.io/en/latest/cloud-setup/cloud-permissions.html#gcp # pylint: disable=line-too-long
-VM_MINIMAL_PERMISSIONS = [
-    "compute.disks.create",
-    "compute.disks.list",
-    "compute.firewalls.create",
-    "compute.firewalls.delete",
-    "compute.firewalls.get",
-    "compute.instances.create",
-    "compute.instances.delete",
-    "compute.instances.get",
-    "compute.instances.list",
-    "compute.instances.setLabels",
-    "compute.instances.setServiceAccount",
-    "compute.instances.start",
-    "compute.instances.stop",
-    "compute.networks.get",
-    "compute.networks.list",
-    "compute.networks.getEffectiveFirewalls",
-    "compute.globalOperations.get",
-    "compute.subnetworks.use",
-    "compute.subnetworks.list",
-    "compute.subnetworks.useExternalIp",
-    "compute.projects.get",
-    "compute.zoneOperations.get",
-    "iam.roles.get",
-    "iam.serviceAccounts.actAs",
-    "iam.serviceAccounts.get",
-    "serviceusage.services.enable",
-    "serviceusage.services.list",
-    "serviceusage.services.use",
-    "resourcemanager.projects.get",
-    "resourcemanager.projects.getIamPolicy",
-]
-
-TPU_MINIMAL_PERMISSIONS = [
-    "tpu.nodes.create",
-    "tpu.nodes.delete",
-    "tpu.nodes.list",
-    "tpu.nodes.get",
-    "tpu.nodes.update",
-    "tpu.operations.get",
 ]

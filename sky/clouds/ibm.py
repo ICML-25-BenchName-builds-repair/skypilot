f1d4a13b17d47153015d01b3020e90b05843b41c
Omer Joshua Cohen <69770646+Cohen-J-Omer@users.noreply.github.com>
2023-04-26 19:09:24 +0300

    IBM cloud provider integration  (#1598)
    * added required ibm references
    
    * support for ibm authentication and vpc client
    
    * marked places first requiring CLI implementation
    
    * node_provider implementation and template file
    
    * IBM's cloud and catalog implementations for sky
    
    * removed redundant check: verified in check.py
    
    * general updates to node provider
    
    * vpc_provider initial integration
    
    * ibm cloud class update
    
    * update authentication process
    
    * update ibm cloud adapter
    
    * remove global vpc client
    
    * small fixes and updates
    
    * major update - vpc provider
    
    * further adjust node provider to vpc provider
    
    * authentication updates relevant file mounts
    
    * add search and tagging clients to ibm adaptor
    
    * support for --r in sky status when cluster stopped
    
    * beginning support for a separate worker profile
    
    * update ibm specific libraries
    
    * support tagging for cluster's vpc
    
    * separate profile for workers on config file
    
    * remove redundant constants (due to vpc tags)
    
    * vpc now terminates after head node
    
    * enable vpc termination when cluster is stopped
    
    * update block_list upon failover
    
    * skypilot adjustment - attach ip to all nodes
    
    * update image to ubuntu 22-4
    
    * replace mockup csv with default data frame
    
    * small update to vpc provider
    
    * node provider supports failed instances
    
    * code style compliance overhaul
    
    * code style adjustments - pylint
    
    * reformatting- yapf
    
    * support ibm testing
    
    * code style fixes
    
    * updates and fixes to cloud/ibm and catalog
    
    * node_provider calculates launch hash
    
    * small fixes and updates
    
    * code style formatter
    
    * tests- added confirmation to sky cancel
    
    * change cluster user from root to ubuntu
    
    * CloudFunctions support - delete cluster remotely
    
    * Return correct value when cluster doesn't exist
    
    * support multi-region images and update user msg
    
    * fix to CloudFunctions implementation
    
    * adaptor conditional importing
    
    * update return value for failed node
    
    * code optimized, docs added,  hybrid ip removed
    
    * 1. moved vm cleanup to vpc module
    2. multithreaded vm removal
    3. new todo added when --image-id implemented
    4. added docs
    
    * include ibm files in formatting
    
    * further adaptations to changes on master
    
    * reformatting code
    
    * added auto confirm to tests
    
    * support --image-id
    
    * mem support on ibm catalog - catch up to master
    
    * delete recyclable ips CloudFunctions&vpc_provider
    
    * mark slow tests
    
    * reformatted files
    
    * make import error uniform with other providers
    
    * switch to adaptor's client on node&vpc_provider
    
    * update ibm tests
    
    * ibm image_id documentation
    
    * remove ibm specific test file
    
    * include IBM with clouds supporting image_id
    
    * move ibm specific imports into function scope
    
    * replace ibm specific funcs with catalog funcs
    
    * fix logic operators
    
    * mark tests that are incompatible with ibm cloud
    
    * update ibm's get_feasible_launchable_resources
    
    * fix logical operator
    
    * removed decorator skipping test for ibm
    
    * satisfy type checkers and formatters
    
    * disk_tier support
    
    * git workflow adaptation to ibm integration
    
    * git workflow fix: format job - syntax error
    
    * classify cluster deletion messages as debug

--- sky/clouds/ibm.py
+++ sky/clouds/ibm.py
@@ -1,19 +1,17 @@
 """IBM Web Services."""
-import json
 import os
-import typing
-from typing import Any, Dict, Iterator, List, Optional, Tuple
-
-import colorama
-
+import yaml
+import json
 from sky import clouds
-from sky import sky_logging
-from sky import status_lib
-from sky.adaptors import ibm
-from sky.adaptors.ibm import CREDENTIAL_FILE
 from sky.clouds import service_catalog
-from sky.utils import ux_utils
+from sky.adaptors import ibm
+from sky import sky_logging
+import typing
+from typing import Dict, Iterator, List, Optional, Tuple
+from sky.adaptors.ibm import CREDENTIAL_FILE
 
+from sky.clouds.cloud import Zone
+from sky.utils import ux_utils
 if typing.TYPE_CHECKING:
     # renaming to avoid shadowing variables
     from sky import resources as resources_lib
@@ -36,21 +34,10 @@ class IBM(clouds.Cloud):
     @classmethod
     def _cloud_unsupported_features(
             cls) -> Dict[clouds.CloudImplementationFeatures, str]:
-        return {
-            clouds.CloudImplementationFeatures.CLONE_DISK_FROM_CLUSTER:
-                (f'Migrating disk is not supported in {cls._REPR}.'),
-            clouds.CloudImplementationFeatures.DOCKER_IMAGE:
-                (f'Docker image is not supported in {cls._REPR}. '
-                 'You can try running docker command inside the '
-                 '`run` section in task.yaml.'),
-            clouds.CloudImplementationFeatures.CUSTOM_DISK_TIER:
-                (f'Custom disk tier is not supported in {cls._REPR}.'),
-            clouds.CloudImplementationFeatures.OPEN_PORTS:
-                (f'Opening ports is not supported in {cls._REPR}.'),
-        }
+        return dict()
 
     @classmethod
-    def max_cluster_name_length(cls) -> Optional[int]:
+    def _max_cluster_name_length(cls) -> Optional[int]:
         return cls._MAX_CLUSTER_NAME_LEN_LIMIT
 
     @classmethod
@@ -59,8 +46,8 @@ class IBM(clouds.Cloud):
                               use_spot: bool, region: Optional[str],
                               zone: Optional[str]) -> List[clouds.Region]:
         del accelerators  # unused
-        if use_spot:
-            return []
+        assert use_spot is False, (
+            'current IBM implementation doesn\'t support spot instances')
         regions = service_catalog.get_region_zones_for_instance_type(
             instance_type, use_spot, 'ibm')
 
@@ -82,7 +69,7 @@ class IBM(clouds.Cloud):
         instance_type: str,
         accelerators: Optional[Dict[str, int]] = None,
         use_spot: bool = False,
-    ) -> Iterator[Optional[List[clouds.Zone]]]:
+    ) -> Iterator[Optional[List[Zone]]]:
         """Loops over (region, zones) to retry for provisioning.
 
         returning a single zone list with its region,
@@ -136,10 +123,10 @@ class IBM(clouds.Cloud):
         # Currently Isn't implemented in the same manner by aws and azure.
         return 0
 
-    def get_egress_cost(self, num_gigabytes: float):
+    def get_egress_cost(self, num_gigabytes):
         """Returns the egress cost. Currently true for us-south, i.e. Dallas.
         based on https://cloud.ibm.com/objectstorage/create#pricing. """
-        cost = 0.
+        cost = 0
         price_thresholds = [{
             'threshold': 150,
             'price_per_gb': 0.05
@@ -163,7 +150,6 @@ class IBM(clouds.Cloud):
     def make_deploy_resources_variables(
         self,
         resources: 'resources_lib.Resources',
-        cluster_name_on_cloud: str,
         region: 'clouds.Region',
         zones: Optional[List['clouds.Zone']],
     ) -> Dict[str, Optional[str]]:
@@ -178,7 +164,6 @@ class IBM(clouds.Cloud):
         Returns:
           A dictionary of cloud-specific node type variables.
         """
-        del cluster_name_on_cloud  # Unused.
 
         def _get_profile_resources(instance_profile):
             """returns a dict representing the
@@ -257,10 +242,17 @@ class IBM(clouds.Cloud):
                                                          disk_tier=disk_tier,
                                                          clouds='ibm')
 
-    def _get_feasible_launchable_resources(
-        self, resources: 'resources_lib.Resources'
-    ) -> Tuple[List['resources_lib.Resources'], List[str]]:
-        fuzzy_candidate_list: List[str] = []
+    def get_feasible_launchable_resources(self,
+                                          resources: 'resources_lib.Resources'):
+        """Returns a list of feasible and launchable resources.
+
+        Feasible resources refer to an offering respecting the resource
+        requirements.  Currently, this function implements "filtering" the
+        cloud's offerings only w.r.t. accelerators constraints.
+
+        Launchable resources require a cloud and an instance type be assigned.
+        """
+        fuzzy_candidate_list: Optional[List[str]] = []
         if resources.instance_type is not None:
             assert resources.is_launchable(), resources
             resources = resources.copy(accelerators=None)
@@ -310,8 +302,8 @@ class IBM(clouds.Cloud):
 
     @classmethod
     def get_default_image(cls, region) -> str:
-        """Returns default image id, currently stock ubuntu 22-04.
-
+        """
+        Returns default image id, currently stock ubuntu 22-04.
         if user specified 'image_id' in ~/.ibm/credentials.yaml
             matching this 'region', returns it instead.
         """
@@ -345,8 +337,7 @@ class IBM(clouds.Cloud):
             and img['operating_system']['architecture'].startswith(
                 'amd')))['id']
 
-    @classmethod
-    def get_image_size(cls, image_id: str, region: Optional[str]) -> float:
+    def get_image_size(self, image_id: str, region: Optional[str]) -> float:
         assert region is not None, (image_id, region)
         client = ibm.client(region=region)
         try:
@@ -355,16 +346,8 @@ class IBM(clouds.Cloud):
         except ibm.ibm_cloud_sdk_core.ApiException as e:  # type: ignore[union-attr]
             logger.error(e.message)
             with ux_utils.print_exception_no_traceback():
-                raise ValueError(
-                    f'Image {image_id!r} not found in IBM region "{region}".\n'
-                    '\nTo use image id in IBM, create a private VPC image and '
-                    'paste its ID in the image_id section.\n'
-                    '\nTo create an image manually:\n'
-                    'https://cloud.ibm.com/docs/vpc?topic=vpc-creating-and-using-an-image-from-volume\n'  # pylint: disable=line-too-long
-                    '\nTo use an official VPC image creation tool:\n'
-                    'https://www.ibm.com/cloud/blog/use-ibm-packer-plugin-to-create-custom-images-on-ibm-cloud-vpc-infrastructure\n'  # pylint: disable=line-too-long
-                    '\nTo use a more limited but easier to manage tool:\n'
-                    'https://github.com/IBM/vpc-img-inst') from None
+                raise ValueError(f'Image {image_id!r} not found in '
+                                 f'IBM region "{region}"') from None
         try:
             # image_size['file']['size'] is not relevant, since
             # the minimum size of a volume onto which this image
@@ -385,32 +368,22 @@ class IBM(clouds.Cloud):
     @classmethod
     def check_credentials(cls) -> Tuple[bool, Optional[str]]:
         """Checks if the user has access credentials to this cloud."""
-
+        # IBM-TODO - create a configuration script.
         required_fields = ['iam_api_key', 'resource_group_id']
-        ibm_cos_fields = ['access_key_id', 'secret_access_key']
         help_str = ('    Store your API key and Resource Group id '
                     f'in {CREDENTIAL_FILE} in the following format:\n'
                     '      iam_api_key: <IAM_API_KEY>\n'
                     '      resource_group_id: <RESOURCE_GROUP_ID>')
-        base_config = ibm.read_credential_file()
 
+        base_config = _read_credential_file()
         if not base_config:
             return (False, 'Missing credential file at '
                     f'{os.path.expanduser(CREDENTIAL_FILE)}.\n' + help_str)
-        # TODO(IBM) update when issue #1943 is resolved.
-        if set(ibm_cos_fields) - set(base_config):
-            logger.debug(f'{colorama.Fore.RED}IBM Storage is missing the '
-                         'following fields in '
-                         f'{os.path.expanduser(CREDENTIAL_FILE)} to function: '
-                         f"""{", ".join(list(
-                            set(ibm_cos_fields) - set(base_config)))}"""
-                         f'{colorama.Style.RESET_ALL}')
 
-        if set(required_fields) - set(base_config):
-            return (
-                False, f'Missing field(s): '
-                f'{", ".join(list(set(required_fields) - set(base_config)))} '
-                f'in {os.path.expanduser(CREDENTIAL_FILE)}.\n{help_str}')
+        for field in required_fields:
+            if field not in base_config:
+                return (False, f'Missing field "{field}" in '
+                        f'{os.path.expanduser(CREDENTIAL_FILE)}.\n' + help_str)
 
         # verifies ability of user to create a client,
         # e.g. bad API KEY.
@@ -450,49 +423,20 @@ class IBM(clouds.Cloud):
         return service_catalog.accelerator_in_region_or_zone(
             accelerator, acc_count, region, zone, 'ibm')
 
-    @classmethod
-    def query_status(cls, name: str, tag_filters: Dict[str, str],
-                     region: Optional[str], zone: Optional[str],
-                     **kwargs) -> List['status_lib.ClusterStatus']:
-        del tag_filters, zone, kwargs  # unused
 
-        status_map: Dict[str, Any] = {
-            'pending': status_lib.ClusterStatus.INIT,
-            'starting': status_lib.ClusterStatus.INIT,
-            'restarting': status_lib.ClusterStatus.INIT,
-            'running': status_lib.ClusterStatus.UP,
-            'stopping': status_lib.ClusterStatus.STOPPED,
-            'stopped': status_lib.ClusterStatus.STOPPED,
-            'deleting': None,
-            'failed': status_lib.ClusterStatus.INIT,
-            'cluster_deleted': []
-        }
-
-        client = ibm.client(region=region)
-        search_client = ibm.search_client()
-        # pylint: disable=E1136
-        vpcs_filtered_by_tags_and_region = search_client.search(
-            query=f'type:vpc AND tags:{name} AND region:{region}',
-            fields=['tags', 'region', 'type'],
-            limit=1000).get_result()['items']
-        if not vpcs_filtered_by_tags_and_region:
-            # a vpc could have been removed unkownlingly to skypilot, such as
-            # via `sky autostop --down`, or simply manually (e.g. via console).
-            logger.warning('No vpc exists in '
-                           f'{region} '
-                           f'with tag: {name}')
-            return status_map['cluster_deleted']
-        vpc_id = vpcs_filtered_by_tags_and_region[0]['crn'].rsplit(':', 1)[-1]
-        instances = client.list_instances(
-            vpc_id=vpc_id).get_result()['instances']
-
-        return [status_map[instance['status']] for instance in instances]
+def _read_credential_file():
+    try:
+        with open(os.path.expanduser(CREDENTIAL_FILE), 'r',
+                  encoding='utf-8') as f:
+            return yaml.safe_load(f)
+    except FileNotFoundError:
+        return False
 
 
 def get_cred_file_field(field, default_val=None) -> str:
     """returns a the value of a field from the user's
      credentials file if exists, else default_val"""
-    base_config = ibm.read_credential_file()
+    base_config = _read_credential_file()
     if not base_config:
         raise FileNotFoundError('Missing '
                                 f'credential file at {CREDENTIAL_FILE}')
--- sky/clouds/service_catalog/ibm_catalog.py
+++ sky/clouds/service_catalog/ibm_catalog.py
@@ -1,15 +1,15 @@
-"""IBM Offerings Catalog.
+"""
+IBM Offerings Catalog.
 
 This module loads the service catalog file and can be used to query
 instance types and pricing information for IBM.
 """
 
-from typing import Dict, List, Optional, Tuple
-
 from sky import sky_logging
-from sky.adaptors import ibm
 from sky.clouds import cloud
 from sky.clouds.service_catalog import common
+from sky.adaptors import ibm
+from typing import Dict, List, Optional, Tuple
 
 logger = sky_logging.init_logger(__name__)
 
@@ -63,8 +63,7 @@ def get_instance_type_for_accelerator(
     region: Optional[str] = None,
     zone: Optional[str] = None,
 ) -> Tuple[Optional[List[str]], List[str]]:
-    """Filter the instance types based on resource requirements.
-
+    """
     Returns a list of instance types satisfying the required count of
     accelerators with sorted prices and a list of candidates with fuzzy search.
     """
@@ -88,13 +87,11 @@ def list_accelerators(
         gpus_only: bool,
         name_filter: Optional[str],
         region_filter: Optional[str],
-        quantity_filter: Optional[int],
         case_sensitive: bool = True
 ) -> Dict[str, List[common.InstanceTypeInfo]]:
     """Returns all instance types in IBM offering accelerators."""
     return common.list_accelerators_impl('IBM', _df, gpus_only, name_filter,
-                                         region_filter, quantity_filter,
-                                         case_sensitive)
+                                         region_filter, case_sensitive)
 
 
 def get_default_instance_type(cpus: Optional[str] = None,

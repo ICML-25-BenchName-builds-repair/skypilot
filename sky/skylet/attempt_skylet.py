191314e8e9288cafedd4a759b39745e92f941130
Mehul Raheja <mraheja@berkeley.edu>
2023-06-06 03:07:53 -0700

    Expanded Job Queue (#1636)
    * working expanded queue
    
    * added event
    
    * updated testing and bug fixes
    
    * fixed missed space
    
    * formatting fix
    
    * fixed setting up status + enabled spot
    
    * format
    
    * fixed setting up test
    
    * addressed comments
    
    * change stop lock range
    
    * error message for needing update
    
    * removed wandb.py
    
    * added skylet restart
    
    * updates
    
    * addressed comments
    
    * formatting + minor comment addressing
    
    * more comments addressed
    
    * Fix rich status
    
    * fixed stalling issue
    
    * format
    
    * small test bug
    
    * fixed skylet launch issue
    
    * formatting + forgetten file
    
    * more formatting + remove check
    
    * addressed comments and removed pkill usage
    
    * schedule after setup too
    
    * Update sky/backends/cloud_vm_ray_backend.py
    
    Co-authored-by: Zhanghao Wu <zhanghao.wu@outlook.com>
    
    * addressed more comments
    
    * formatting
    
    * Address comments
    
    * faster job scheduling
    
    * format
    
    * Fix cancellation logic
    
    * Don't schedule a job after reboot
    
    * add comment
    
    * revert changes in test
    
    * Add test for cancelling pending jobs
    
    * Make update job status more readable
    
    * schedule more frequently for job cancelling
    
    ---------
    
    Co-authored-by: Zhanghao Wu <zhanghao.wu@outlook.com>

--- sky/skylet/attempt_skylet.py
+++ sky/skylet/attempt_skylet.py
@@ -9,9 +9,7 @@ VERSION_FILE = os.path.expanduser(constants.SKYLET_VERSION_FILE)
 
 
 def restart_skylet():
-    # Kills old skylet if it is running.
-    # TODO(zhwu): make the killing graceful, e.g., use a signal to tell
-    # skylet to exit, instead of directly killing it.
+    # Kills old skylet if it is running
     subprocess.run(
         'ps aux | grep "sky.skylet.skylet" | grep "python3 -m"'
         '| awk \'{print $2}\' | xargs kill >> ~/.sky/skylet.log 2>&1',
@@ -34,24 +32,10 @@ proc = subprocess.run(
 running = (proc.returncode == 0)
 
 version_match = False
-found_version = None
 if os.path.exists(VERSION_FILE):
     with open(VERSION_FILE) as f:
-        found_version = f.read().strip()
-        if found_version == constants.SKYLET_VERSION:
+        if f.read().strip() == constants.SKYLET_VERSION:
             version_match = True
-
-version_string = (f' (found version {found_version}, new version '
-                  f'{constants.SKYLET_VERSION})')
-if not running:
-    print('Skylet is not running. Starting (version '
-          f'{constants.SKYLET_VERSION})...')
-elif not version_match:
-    print(f'Skylet is stale{version_string}. Restarting...')
-else:
-    print(
-        f'Skylet is running with the latest version {constants.SKYLET_VERSION}.'
-    )
 
 if not running or not version_match:
     restart_skylet()

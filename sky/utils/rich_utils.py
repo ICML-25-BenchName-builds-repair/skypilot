eab99c60d3b61bffef2bf57099774bca557f301f
Zhanghao Wu <zhanghao.wu@outlook.com>
2023-08-30 19:49:33 -0700

    [UX] Make logger safe with rich status (#2486)
    * Make logger safe with rich status
    
    * Fix restarting issue with the spinner
    
    * lint
    
    * renaming
    
    * remove unnecessary status stop
    
    * lint
    
    * remove direct status usage
    
    * format
    
    * only lock when _status is not None
    
    * small fixes

--- sky/utils/rich_utils.py
+++ sky/utils/rich_utils.py
@@ -1,7 +1,6 @@
 """Rich status spinner utils."""
 import contextlib
 import threading
-from typing import Union
 
 import rich.console as rich_console
 
@@ -15,7 +14,7 @@ class _NoOpConsoleStatus:
     """An empty class for multi-threaded console.status."""
 
     def __enter__(self):
-        return self
+        pass
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         pass
@@ -30,7 +29,7 @@ class _NoOpConsoleStatus:
         pass
 
 
-def safe_status(msg: str) -> Union['rich_console.Status', _NoOpConsoleStatus]:
+def safe_status(msg: str):
     """A wrapper for multi-threaded console.status."""
     from sky import sky_logging  # pylint: disable=import-outside-toplevel
     if (threading.current_thread() is threading.main_thread() and

b88a7e52117c9797ef5d06d2f606f5092776e103
Tian Xia <cblmemo@gmail.com>
2023-09-17 20:24:19 -0700

    [Provisioner] Update ports for UP cluster (#2485)
    * AWS is working
    
    * GCP finished. TODO: find a way to write cluster yaml in backends_utils
    
    * simplify ports when repr
    
    * generate new sg for aws
    
    * fix aws new sg no permission for ssh, format
    
    * remove finished TODO
    
    * write cluster config in backend_utils; deprecate code in config.py
    
    * remove redundant
    
    * minor
    
    * format
    
    * nit
    
    * nit
    
    * Azure finished
    
    * fix
    
    * lint
    
    * change port type to List[str]
    
    * add cli option
    
    * minor
    
    * add doc
    
    * add doc
    
    * add ports doc
    
    * Update docs/source/reference/yaml-spec.rst
    
    Co-authored-by: Romil Bhardwaj <romil.bhardwaj@berkeley.edu>
    
    * add port doc
    
    * apply suggestions from code review
    
    * Apply suggestions from code review
    
    Co-authored-by: Zhanghao Wu <zhanghao.wu@outlook.com>
    
    * fix
    
    * upd docs
    
    * apply suggestions from code review
    
    * change api to all ports, fix bug, check ports in resource_utils
    
    * minor
    
    * gracefully handle gcp ports
    
    * Update docs/source/reference/yaml-spec.rst
    
    Co-authored-by: Zhanghao Wu <zhanghao.wu@outlook.com>
    
    * upd doc
    
    * remove ports argument & move ports-specific variable to make_deploy_variables
    
    * restore ToProvisionConfig
    
    * remove new provisioner api check
    
    * add checking for whether need open ports
    
    * nits & ffix multi-node cluster in aws
    
    * add backward compatibility for gcp ports
    
    * merge get_vpc_name and create_or_update_firewall_rule
    
    * fix __setstate__
    
    * get rid of DEFAULT_AWS_SG_NAME and fix a bug
    
    * nits
    
    * nits
    
    * constant for aws default sg
    
    * move ports delta calculation into _update_after_cluster_provisioned
    
    * move sg / firewall name generation into make_deploy_variables
    
    * nit
    
    * fix
    
    * nits
    
    * update doc
    
    * fix a bug
    
    * Apply suggestions from code review
    
    Co-authored-by: Romil Bhardwaj <romil.bhardwaj@berkeley.edu>
    
    * add minimal rule for aws
    
    * add minimal for gcp
    
    * fix
    
    ---------
    
    Co-authored-by: Romil Bhardwaj <romil.bhardwaj@berkeley.edu>
    Co-authored-by: Zhanghao Wu <zhanghao.wu@outlook.com>

--- sky/provision/azure/instance.py
+++ sky/provision/azure/instance.py
@@ -3,7 +3,6 @@ from typing import Any, Callable, Dict, List, Optional
 
 from sky import sky_logging
 from sky.adaptors import azure
-from sky.utils import ux_utils
 
 logger = sky_logging.init_logger(__name__)
 
@@ -67,9 +66,8 @@ def open_ports(
                 ))
             update_network_security_groups(resource_group, nsg.name, nsg)
         except azure.http_error_exception() as e:
-            with ux_utils.print_exception_no_traceback():
-                raise ValueError(
-                    f'Failed to open ports {ports} in NSG {nsg.name}.') from e
+            logger.warning(
+                f'Failed to open ports {ports} in NSG {nsg.name}: {e}')
 
 
 def cleanup_ports(
